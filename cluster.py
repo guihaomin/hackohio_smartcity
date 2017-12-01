01207430d2d6e7875a1ed489385ebe230eb185f6
guihaomin <gui.29@osu.edu>
2017-12-01 10:43:56 +0800

    1. update cluster.

--- cluster.py
+++ cluster.py
@@ -2,19 +2,40 @@ import numpy as np
 from sklearn.cluster import KMeans
 
 a=np.load("speeddata.npy")
-y_pred = KMeans(n_clusters=3, random_state=170).fit_predict(a)
+y_pred = KMeans(n_clusters=5, random_state=170).fit_predict(a)
 import matplotlib.pyplot as plt
 X=[]
 Y=[]
 Xd=[]
 Yd=[]
 for item in a:
-    X.append(item[1])
-    Y.append(item[2])
-    Xd.append(item[3])
-    Yd.append(item[4])
-plt.scatter(X, Y, c=y_pred)
+    X.append(item[2])
+    Y.append(item[1])
+    Xd.append(item[4])
+    Yd.append(item[3])
+color=[]
+hypoX=[]
+hypoY=[]
+for i in range(0,len(y_pred)):
+    if(y_pred[i]==0):
+        color.append("red")
+        hypoX.append(a[i][2])
+        hypoY.append(a[i][1])
+    elif(y_pred[i]==1):
+        color.append("white")
+     #  hypoX.append(a[i][2])
+     #  hypoY.append(a[i][1])
+    elif(y_pred[i]==2):
+        color.append("white")
+    elif(y_pred[i]==3):
+        color.append("white")
+      # hypoX.append(a[i][2]) #copy this 2 lines aroung to see different cluster
+      # hypoY.append(a[i][1])
+    elif(y_pred[i]==4):
+        color.append("white")
+#plt.scatter(X, Y, c=color)
+plt.scatter(hypoX, hypoY)
 plt.show()
 plt.clf()
-plt.scatter(Xd, Yd, c=y_pred)
+#plt.scatter(Xd, Yd, c=y_pred)
 plt.show()
